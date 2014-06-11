9beb516e69d0407666730dcd80efcf9e681793dd
Alexander Wong Makers <mavrm@hotmail.com>
2014-06-11 17:18:27 +0100

    update the title after ajax

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,6 +15,16 @@ $(document).ready(function(){
 			success: function(response){
 				console.log(response);
 				$(form).closest('.panel-collapse').collapse('toggle');
+				var headers = ["title", "deadline", "importance", "difficulty"];
+				var newText = "";
+				for (var i in headers){
+					var header = headers[i];
+					newText += (response[header]+" ");
+				}
+				
+
+				$(form).closest('article').find('a').text(newText);
+				
 			}
 		});
 	});
