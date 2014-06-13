7485e2b1b1131be5517dc868a10aa4627ed86d02
Joey Wolf <josephwolf.uk@gmail.com>
2014-06-13 16:57:25 +0100

    fixed database default bug

--- db/migrate/20140613155611_change_column_default.rb
+++ db/migrate/20140613155611_change_column_default.rb
@@ -1,4 +1,5 @@
 class ChangeColumnDefault < ActiveRecord::Migration
   def change
+  	change_column_default :tasks, :category, 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613152039) do
+ActiveRecord::Schema.define(version: 20140613155611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140613152039) do
     t.date     "completedAt"
     t.integer  "user_id"
     t.text     "description"
-    t.integer  "category"
+    t.integer  "category",    default: 1
   end
 
   create_table "users", force: true do |t|
