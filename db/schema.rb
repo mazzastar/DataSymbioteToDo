44eb7b0b17f78396e9b70ea3abf749a3246bc81a
Joey Wolf <josephwolf.uk@gmail.com>
2014-06-13 16:21:55 +0100

    set category default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612144256) do
+ActiveRecord::Schema.define(version: 20140613152039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
